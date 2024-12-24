e6a06b97b7b9b8913ea9a5de552611fd4cadcf56
sujinp <sujinp038@naver.com>
2024-12-24 17:52:41 +0900

    1224수업

--- javawork/NaverjavaProject/src/day1223/P320.java
+++ javawork/NaverjavaProject/src/day1223/P320.java
@@ -1,23 +1,39 @@
 package day1223;
 
- class Vehicle{
-	//메소드 선언
-	public void run() {
-		System.out.println("차량이 달립니다.");
-	}
+//class Vehicle{
+//	//메소드 선언
+//	public void run() 
+//	{
+//		System.out.println("차량이 달립니다.");
+//	}
+//}
+
+//abstract메서드에는 추상메서드도 일반메서드도 들어갈 수 있음 
+//abstract class Vehicle
+//{
+//	public abstract void run();
+//}
+
+//어차피 추상메서드만 있는 경우 interface 로 변경 가능함
+interface Vehicle
+{
+	public void run();
 }
 
- class Bus extends Vehicle
+
+// class Bus extends Vehicle
+class Bus implements Vehicle
 {
 	//메소드 재정의(오버라이딩)
 	@Override
-	public void run() {
+	public void run() 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("버스가 달립니다.");
 	}
 }
 
- class Taxi extends Vehicle
+ class Taxi implements Vehicle
 {
 	@Override
 	public void run() {
@@ -25,16 +41,16 @@ package day1223;
 		System.out.println("택시가 달립니다.");
 	}
 }
-public class P320 {
-
-	static class Driver
+ class Driver//static이면 driver.driv로 호출 가능함
 	{
-		//메소드 선언(클래스 타입의 매개변수를 가지고 있음)
+		//메소드 선언(클래스 타입의 매개변수를 가지고 있음) > 매개변수 다형성
 		public void drive(Vehicle vehicle)
 		{
 			vehicle.run();
 		}
 	}
+ 
+public class P320 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ javawork/NaverjavaProject/src/day1224/Ex7Map.java
@@ -0,0 +1,37 @@
+package day1224;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Ex7Map {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Map 타입은 Key와 value의 쌍으로 데이터를 추가한다.
+		//이때 key값은 Set 타입이다. 즉, 중복허용하지 않고 비순자척으로 나열한다.
+		Map<String, String> map=new HashMap<String, String>();//(키값, value값)각각 변수타입줘야함
+		//map에 데이터 추가
+		map.put("name", "송혜교");
+		map.put("age", "34");
+		map.put("blood", "AB");
+		map.put("name", "이영자");//key값이 중복이면 가장 나중값이 들어감
+		
+		System.out.println("map의  사이즈: "+map.size());
+		System.out.println("map에서 key값으로 value 값 꺼내기");
+		System.out.println("이름:"+map.get("name"));
+		System.out.println("나이:"+map.get("age"));
+		System.out.println("혈액형:"+map.get("blood"));
+		System.out.println("핸드폰:"+map.get("hp"));//없을경우 null값 반환
+		System.out.println();
+		System.out.println("Key 값들을 자동으로 얻은 후  value 값들 얻기");
+		Set<String> keySets=map.keySet();
+		for(String key:keySets)
+		{
+			String v=map.get(key);
+			System.out.println(key+"=>"+v);
+		}
+		
+	}
+
+}
