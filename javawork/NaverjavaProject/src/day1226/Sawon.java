38dc6b8866e84c23c0ef577633b85f7bff0affd8
sujinp <sujinp038@naver.com>
2024-12-26 18:53:52 +0900

    1226수업

---
+++ javawork/NaverjavaProject/src/day1226/Sawon.java
@@ -0,0 +1,56 @@
+package day1226;
+
+public class Sawon {
+	
+	private String sawonName;
+	private int age;
+	private String hp;
+	private String address;
+	
+	public Sawon() {
+		
+	}
+
+	public Sawon(String sawonName, int age, String hp, String address) {
+		super();//상속이 아닌데도 있는건 크게 신경 안써도 됨
+		this.sawonName = sawonName;
+		this.age = age;
+		this.hp = hp;
+		this.address = address;
+	}
+
+	public String getSawonName() {
+		return sawonName;
+	}
+
+	public void setSawonName(String sawonName) {
+		this.sawonName = sawonName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
