c8b9e88807ead2d43e7f94ff1e87f3ca91162d3a
sujinp <sujinp038@naver.com>
2024-12-30 18:31:25 +0900

    1230수업

--- javawork/NaverjavaProject/src/day1226/Ex6TableCRUD.java
+++ javawork/NaverjavaProject/src/day1226/Ex6TableCRUD.java
@@ -1,13 +1,17 @@
 package day1226;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -93,6 +97,30 @@ public class Ex6TableCRUD extends JFrame {
 			}
 		}
 	}
+	//List의 데이터를 테이블에 출력해주는 메서드
+	public void writeTableData()
+	{
+		//기존의 테이블에 출력된 데이터를 삭제 후 다시 추가
+		tableModel.setRowCount(0);
+		
+		for(Student stu:list)//list에 있는 student를 뽑아서 vector에 넣을거야!
+		{
+			Vector<String> data=new Vector<String>();//매번 넣을때마다 새로생성되어야함
+			int kor=stu.getKor();
+			int eng=stu.getEng();
+			int sum=kor+eng;
+			double avg=sum/2.0;
+			
+			data.add(stu.getName());
+			data.add(String.valueOf(kor));
+			data.add(String.valueOf(eng));
+			data.add(String.valueOf(sum));
+			data.add(String.valueOf(avg));
+			
+			//table에 추가(추가하는 메서드도 모델이 갖고있음)
+			tableModel.addRow(data);
+		}
+	}
 	
 	public void initDesign()
 	{
@@ -103,6 +131,10 @@ public class Ex6TableCRUD extends JFrame {
 		String []title= {"이름","국어","영어","총점","평균"};
 		tableModel=new DefaultTableModel(title, 0);//일단 행갯수는 0으로 생성
 		table=new JTable(tableModel);
+		
+		//table에 데이터 추가하기>위에서 table 테이블 생성된 후에 데이터 추가
+		this.writeTableData();
+		
 		//frame에 추가
 		this.add("Center",new JScrollPane(table));
 		
@@ -113,6 +145,33 @@ public class Ex6TableCRUD extends JFrame {
 		tfEng=new JTextField(4);
 		
 		btnAdd=new JButton("추가");
+		
+		//버튼 클릭시 이벤트 발생
+		btnAdd.addActionListener(new ActionListener() {//이 안에서 this쓰면 ActionListener이걸 호출하는 것임
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//입력한 데이터를 읽이서 student에 넣은 후 다시 list에 추가
+				String name=tfName.getText();
+				int kor=Integer.parseInt(tfKor.getText());//이렇게되면 kor에 문자 넣으면 numberformatException발생
+				int eng=Integer.parseInt(tfEng.getText());//getText는 반환타입이 무조건 String이리서 Integer.parseInt로 변환해줘야함
+				
+				Student stu=new Student(name,kor,eng);
+				list.add(stu);
+				
+				//table 다시 출력 > 입력한 데이터를 table에 넣어서 재출력하는거얌
+				writeTableData();
+				
+				//입력데이터는 지우기
+				tfName.setText("");
+				tfKor.setText("");
+				tfEng.setText("");
+				
+			}
+		});
+		
+		//panel에 각 컴포넌트들 추가
 		
 		panel.add(new JLabel("이름"));
 		panel.add(tfName);
@@ -130,6 +189,30 @@ public class Ex6TableCRUD extends JFrame {
 	public void saveFile()
 	{
 		//List의 내용을 파일에 저장한다.
+		FileWriter fw=null;
+		
+		try {
+			fw=new FileWriter(FILENAME);
+			
+			for(Student stu:list)
+			{
+				String s=stu.getName()+"|"+stu.getKor()+"|"+stu.getEng()+"\n";
+				//파일에 저장
+				fw.write(s);
+				
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				fw.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	
